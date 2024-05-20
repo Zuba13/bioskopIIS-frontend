9a3d722b726683e7c48b8b66f4992a71f906d439
Breskva <vukmacanovic@gmail.com>
2024-05-20 17:35:46 +0200

    Delete unused modules

--- Bioskop/src/app/app.module.ts
+++ Bioskop/src/app/app.module.ts
@@ -8,10 +8,6 @@ import { LayoutModule } from './feature-modules/layout/layout.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './infrastructure/material/material.module';
 import { AdministrationModule } from './feature-modules/administration/administration.module';
-import { BlogModule } from './feature-modules/blog/blog.module';
-import { MarketplaceModule } from './feature-modules/marketplace/marketplace.module';
-import { TourAuthoringModule } from './feature-modules/tour-authoring/tour-authoring.module';
-import { TourExecutionModule } from './feature-modules/tour-execution/tour-execution.module';
 import { AuthModule } from './infrastructure/auth/auth.module';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { JwtInterceptor } from './infrastructure/auth/jwt/jwt.interceptor';
@@ -29,10 +25,6 @@ import { FoodBeverageModule } from './feature-modules/food-beverage/food-beverag
     BrowserAnimationsModule,
     MaterialModule,
     AdministrationModule,
-    BlogModule,
-    MarketplaceModule,
-    TourAuthoringModule,
-    TourExecutionModule,
     AuthModule,
     HttpClientModule,
     MatSnackBarModule,
